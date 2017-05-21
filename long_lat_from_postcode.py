8fd5f6a62ae4d6971e1f9de03f0c998153bff0a0
Chris Hawkins <Chris Hawkins>
2017-05-21 20:50:55 +0100

    Create grid of coordinates added

--- long_lat_from_postcode.py
+++ long_lat_from_postcode.py
@@ -3,30 +3,60 @@
 
 import urllib.request, json 
 
-class long_lat(object):
+def find_long_lat(postcode):
     '''
-    from a user entered postcode I need to return a longditude and lattitude. I use the api api.postcodes
+    This finds the long lat from a postcode using an api called postcodes. I want to output the result as a list 
     '''
-    def __init__(self, postcode, long_lat_result = []):
-        self.postcode = postcode
-        self.long_lat_result = long_lat_result
-        
-    def find_long_lat(self):
-        ''' using postcode I retrieve the long and lat as a list of strings '''
-        with urllib.request.urlopen("http://api.postcodes.io/postcodes/" + self.postcode) as url:
-            data = json.loads(url.read().decode())
-            
-        longd = data['result']['longitude']
-        lat = data['result']['latitude']
-        self.long_lat_result.append(longd)
-        self.long_lat_result.append(lat)
+    # Download the json data from api.postcodes
+    with urllib.request.urlopen("http://api.postcodes.io/postcodes/" + postcode) as url:
+        data = json.loads(url.read().decode())
+
+    # create the output list and append latt
+    long_and_lat = []
+    long_and_lat.append(data['result']['latitude'])
+    long_and_lat.append(data['result']['longitude'])
+    
+    return long_and_lat
+
+
+def create_long_lat_grid(long_and_lat):
+    '''
+    Take a single long/lat coordinate and make a grid around that coordinate.  
+    '''
+    # create a coordinate at the bottom right of the square I want.
+    long_and_lat[0] = long_and_lat[0] - float(0.005)
+    long_and_lat[1] = long_and_lat[1] + float(0.005)
+               
+    # all the values in the square are going to be saved here. 
+    long_lat_grid = []
+
+    # Move up one 0.001 10 times. Add that value to my list. 
+    for _ in range(10):
+        long_and_lat[0] =   long_and_lat[0] + float(0.001)
+        long_lat_grid.append(long_and_lat.copy())
  
-        return self.long_lat_result
+        # move left 0.001 10 times. again add this value to my list. 
+        for _ in range(10):
+            long_and_lat[1] =   long_and_lat[1] - float(0.001)
+            long_lat_grid.append(long_and_lat.copy())
+        
+        # once I've done the whole bottom row start again at the right.     
+        long_and_lat[1] = long_and_lat[1] + float(0.01)
+
+    return long_lat_grid
+       
+    
+def():
+    ''' I'm going to look through every long lat in my grid from create_long_lat_grid and run it for every month in the last x months'''
+    
 
 
 
 
-# In[ ]:
 
+postcode = 'SW1A2HJ'
+long_lat_result = find_long_lat(postcode)  
+print (long_lat_result)
 
+grid_result = create_long_lat_grid(long_lat_result)
 
--- test_crimes.py
+++ test_crimes.py
@@ -6,21 +6,30 @@
 import unittest
 
 
-from long_lat_from_postcode import long_lat
+from long_lat_from_postcode import find_long_lat, create_long_lat_grid
 
 class LongLatTestCase(unittest.TestCase):
     ''' test for long_lat_from_postcode.py'''
     
     def test_get_correct_Long_Lat(self):
         ''' do I get [-0.125668386000805, 51.506582950758] for this postcode?'''
-        correct_postcode = [-0.125668386000805, 51.506582950758]
-        
+        long_and_lat = [51.506582950758, -0.125668386000805]
+     				
         postcode = 'SW1A2HJ'
-        result = long_lat(postcode)
-        post_result = result.find_long_lat()
-        assert(post_result == correct_postcode)
+        result = find_long_lat(postcode)
+    
+        assert(result == long_and_lat)
 
-        
+    def test_create_long_lat_grid(self):
+    	''' test create_long_lat_grid I'm going check that the length is the same and do a view spot checks '''
+    	long_and_lat = [51.506582950758, -0.125668386000805]
+    	result = create_long_lat_grid(long_and_lat)
+    	correct_lat_value = [51.511582950757976, -0.130668386000805]
+    	assert(result[-1] == correct_lat_value)
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
 
